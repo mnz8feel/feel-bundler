ad2496684883fb61014927a0fea5f89d4e1d2ed8
mnz8 <mnz8gg@gmail.com>
2024-07-01 00:46:15 +0800

    feat: farm rspack mako vite

---
+++ mako/src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './App.module.css';
+
+function App() {
+  return <div className={styles.title}>Hello Mako!</div>;
+}
+
+export { App };
---
+++ track.md
@@ -0,0 +1,17 @@
+pnpm create farm
+
+pnpm create mako
+
+pnpm create rsbuild@latest
+
+pnpm create vite
+
+## to be continued
+
+turbopack
+
+rolldown for vite
+
+## legend
+
+webpack
